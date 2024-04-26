f7bbb3d2ea0f915baa1d6f9550cf4a416b05a65a
Wim Bervoets <wim.bervoets@pxl.be>
2024-04-26 11:45:38 +0200

    added CounterStore

--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@ import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
 import '../scss/main.scss'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const pinia = createPinia()
 
 app.use(router)
+app.use(pinia)
 
 app.mount('#app')
