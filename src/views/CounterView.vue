6323ba8cae864e03ce7d14a562e8453d3799d571
Ashraf Ismail <114289521+pxlashrafismail@users.noreply.github.com>
2024-06-09 06:21:04 +0200

    deel-2 from terminal

--- src/views/CounterView.vue
+++ src/views/CounterView.vue
@@ -2,9 +2,9 @@
     <div class="mx-auto w-25 counter container d-flex justify-content-center flex-column">
         <h2>Counter: </h2>
         <p class=" my-2 border border-primary">
-            {{ counter }}
-            <span v-if="isEven"> == Even</span>
-            <span v-if="!isEven"> != Oneven</span>
+            {{ counterStore.counter }}
+            <span v-if="this.counterStore.isEven"> == Even</span>
+            <span v-if="!this.counterStore.isEven"> != Oneven</span>
         </p>
         <button class="btn btn-primary my-2 text-light" @click="increment()">+ 1</button>
         <button class="btn btn-primary my-2 text-light" @click="decrement()">- 1</button>
@@ -12,24 +12,20 @@
 </template>
 
 <script>
+import { useCounterStore } from '@/stores/CounterStore.js';
 export default {
     data() {
         return {
-            counter: 0 
+            counterStore : useCounterStore(),
         }
     },
     methods: {
         increment() {
-            this.counter++; 
+            this.counterStore.increment(); 
         },
         decrement() {
-            this.counter--; 
+            this.counterStore.decrement(); 
         }
-    },  
-    computed: {
-        isEven() {
-            return this.counter % 2 === 0; 
-        }
-    } 
+    },
 }
 </script>
